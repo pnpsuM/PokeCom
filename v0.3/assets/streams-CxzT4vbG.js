e8070d7202cc2866a9f1039603dd4efb7418c145
pnpsuM <ajy9812@gmail.com>
2026-09-26 12:05:21 +0900

    Delete v0.3 directory

--- v0.3/assets/streams-CxzT4vbG.js
+++
@@ -1,1 +0,0 @@
-import"./index-BtLjgnUr.js";
--- v0.3/sw.js
+++
@@ -1,8 +0,0 @@
-const C='pokecom-v0.3';
-const CORE=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','assets/index-BtLjgnUr.js','assets/jszip.min-6BoigJLs.js','assets/streams-CxzT4vbG.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C&&k.startsWith('pokecom-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET')return;
- if(u.origin===location.origin){e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{if(res.ok){const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl))}return res})));}
- else if(u.hostname.endsWith('googleapis.com')||u.hostname.endsWith('gstatic.com')){e.respondWith(caches.open(C+'-fonts').then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(res=>{c.put(e.request,res.clone());return res}))));}
-});
