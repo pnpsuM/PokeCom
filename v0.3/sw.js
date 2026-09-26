201e7f3fba0108ab2653cf369722c85224faf4f3
pnpsuM <ajy9812@gmail.com>
2026-09-26 10:34:46 +0900

    M1.2 Update
    improved AI / Rating system

--- v0.3/sw.js
+++ v0.3/sw.js
@@ -1,5 +1,5 @@
-const C='pokecom-m1-v1';
-const CORE=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','assets/index-CEpc4yJ2.js','assets/jszip.min-DyJiNn0C.js','assets/streams-D8zS4w6j.js'];
+const C='pokecom-v0.3';
+const CORE=['./','index.html','manifest.webmanifest','icon.svg','icon-192.png','assets/index-BtLjgnUr.js','assets/jszip.min-6BoigJLs.js','assets/streams-CxzT4vbG.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C&&k.startsWith('pokecom-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET')return;
